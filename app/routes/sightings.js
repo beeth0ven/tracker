08b3dca13d648c8f9b4f8ea6c602d3d098fe15e7
beeth0ven <beeth0vendev@163.com>
2017-07-01 23:44:55 +0800

    setup

---
+++ app/routes/sightings.js
@@ -0,0 +1,38 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  module() {
+    return [
+      {
+        id: 1,
+        location: 'Asilomar',
+        sightedAt: new Date('2016-3-07')
+      },
+      {
+        id: 2,
+        location: 'Asilomar',
+        sightedAt: new Date('2016-3-07')
+      },
+      {
+        id: 3,
+        location: 'Asilomar',
+        sightedAt: new Date('2016-3-07')
+      },
+      {
+        id: 4,
+        location: 'Asilomar',
+        sightedAt: new Date('2016-3-07')
+      },
+      {
+        id: 5,
+        location: 'Asilomar',
+        sightedAt: new Date('2016-3-07')
+      },
+      {
+        id: 6,
+        location: 'Asilomar',
+        sightedAt: new Date('2016-3-07')
+      }
+    ];
+  }
+});
