ce7f221b3182b539711eee90bc4de135c2ad14fb
beeth0ven <beeth0vendev@163.com>
2017-07-01 11:56:43 +0800

    setup

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,18 @@
 /*jshint node:true*/
 /* global require, module */
+"use strict";
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
+var bootstrapPath = 'bower_components/bootstrap-sass/assets/';
+
 module.exports = function(defaults) {
+
   var app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      includePaths: [
+        bootstrapPath + 'stylesheets'
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -19,6 +27,7 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import(bootstrapPath + 'javascripts/bootstrap.js');
 
   return app.toTree();
 };
