2e44945a479121db43eafee37cb578817b4b7f84
beeth0ven <beeth0vendev@163.com>
2017-07-04 21:16:44 +0800

    Add transitions.

--- app/helpers/moment-from.js
+++ app/helpers/moment-from.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export function momentFrom(params) {
   let time = window.moment(...params);
-  let formatted = time.format('MMMM Do YYYY');
+  let formatted = time.fromNow();
   return new Ember.Handlebars.SafeString(
     '<span class="text-primary">' + formatted + '</span>'
   );
