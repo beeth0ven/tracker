5aa9802a69f719a89c4c7b408839070cad0b27a7
beeth0ven <beeth0vendev@163.com>
2017-07-05 22:41:27 +0800

    finish controller

--- app/routes/sightings.js
+++ app/routes/sightings.js
@@ -2,20 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    let record1 = this.store.createRecord('sighting', {
-      location: 'Atlanta',
-      sightedAt: new Date('2016-3-07')
-    });
-    record1.set('location', 'Paris, France');
-    console.log('Record 1 location: ' + record1.get('location'));
-    let record2 = this.store.createRecord('sighting', {
-      location: 'Calloway',
-      sightedAt: new Date('2016-3-07')
-    });
-    let record3 = this.store.createRecord('sighting', {
-      location: '',
-      sightedAt: new Date('2016-3-07')
-    });
-    return [record1, record2, record3];
+    return this.store.findAll('sighting', {reload: true});
   }
 });
--- app/routes/sightings/new.js
+++ app/routes/sightings/new.js
@@ -1,4 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      sighting: this.store.createRecord('sighting'),
+      cryptids: this.store.findAll('cryptid'),
+      witnesses: this.store.findAll
+    });
+  },
+  sighting: Ember.computed.alias('controller.model.sighting'),
+  actions: {
+    willTransition() {
+      let sighting = this.get('controller.model.sighting');
+      if (sighting.get('hasDirtyAttributes')) {
+        sighting.deleteRecord();
+      }
+    },
+    create() {
+      let self = this;
+      self.get('sighting')
+        .save()
+        .then(function () {
+          self.transitionToRoute('sightings');
+        });
+    },
+    cancel() {
+      this.get('sighting').deleteRecord();
+      this.transitionToRoute('sightings');
+    }
+  }
 });
